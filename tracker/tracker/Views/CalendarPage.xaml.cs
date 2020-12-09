89e274012dc46b28429a71e4e19084766b059b8b
avacodehub <avacodehub@gmail.com>
2020-12-09 18:35:35 +0300

    Export and share DaysTotalTime as CSV feature

--- tracker/tracker/Views/CalendarPage.xaml.cs
+++ tracker/tracker/Views/CalendarPage.xaml.cs
@@ -8,6 +8,11 @@ using tracker.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Plugin.Calendar.Models;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using Xamarin.Essentials;
+using CsvHelper.Configuration;
 
 namespace tracker.Views
 {
@@ -15,6 +20,7 @@ namespace tracker.Views
     public partial class CalendarPage : ContentPage
     {
         public ICommand DayTappedCommand => new Command<DateTime>((date) => DayTapped(date));
+        public ICommand ExportTappedCommand => new Command(async () => await ExportAndShare());
         public EventCollection Days { get; set; }
         public List<Session> Sessions { get; set; }
         public Project LocalProject { get; set; }
@@ -56,9 +62,65 @@ namespace tracker.Views
 
         }
 
-        private void barExportClicked(object sender, EventArgs e)
+        private async Task ExportAndShare()
         {
+            if (Days.Count == 0)
+            {
+                await DisplayAlert("Export CSV", "This project doesn't have data to export yet", "OK");
+                return;
+            }
+            /*string path = Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+                            , App.CSV_EXPORT_TMP_FILE);*/
 
+            var path = Path.Combine(FileSystem.CacheDirectory, App.CSV_EXPORT_TMP_FILE);
+
+            try { 
+                File.Delete(path);
+            }
+            catch { }
+
+            using (var writer = new StreamWriter(path))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.RegisterClassMap<DayMap>();
+                csv.WriteRecords(GetDaysTotalTime());
+            }
+
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                Title = "Send CSV",
+                File = new ShareFile(path)
+            });
+        }
+
+        public sealed class DayMap : ClassMap<KeyValuePair<string, TimeSpan>>
+        {
+            public DayMap()
+            {
+                Map(m => m.Key).Name("Date");
+                Map(m => m.Value).Name("Total time");
+            }
+        }
+
+        public Dictionary<string, TimeSpan> GetDaysTotalTime()
+        {
+            var _daysTotalTime = new Dictionary<string, TimeSpan>();
+            foreach (var d in Days)
+            {
+                string day_string = d.Key.ToString("D");
+
+                if (!_daysTotalTime.ContainsKey(day_string))
+                {
+                    _daysTotalTime.Add(day_string, new TimeSpan(0, 0, 0));
+                }
+
+                foreach (Session s in d.Value)
+                {
+                    _daysTotalTime[day_string] += s.Duration;
+                }
+            }
+            return _daysTotalTime;
         }
 
         private void DayTapped(DateTime date)
